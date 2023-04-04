edf04811fa28915263d76f74b06759814e4a8a26
Pavel <impetus@yandex.ru>
2023-04-04 17:54:39 +0300

    add exception

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.MissingServletRequestParameterException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.io.Writer;
 
 @Controller
 public class MyController {
@@ -26,5 +29,15 @@ public class MyController {
     @GetMapping("/list")
     public @ResponseBody Iterable<Pizza> getAll() {
         return repository.findAll();
+    }
+
+
+    @ExceptionHandler(Throwable.class)
+    @ResponseBody
+    public void handleException(final Exception e, final HttpServletRequest request,
+                                Writer writer) throws IOException {
+        writer.write(String.format(
+                "{\"error\":{\"java.class\":\"%s\", \"message\":\"%s\"}}",
+                e.getClass(), e.getMessage()));
     }
 }
